de3001f014cf2d108d7df8f001b6413accfcd0e2
Nirmal@6798 <nirmalnemade8864@gmail.com>
2020-09-29 16:01:12 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UserComponent } from './user/user.component'
+import { UserdetailsComponent } from './userdetails/userdetails.component'
+import { DemoComponent } from './demo/demo.component' 
+import { InfoComponent } from './info/info.component'
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: 'user',
+    component: UserComponent
+  },
+  {
+    path: "userdetails",
+    component: UserdetailsComponent
+  },
+  {
+    path: "demo",
+    component: DemoComponent
+  },
+  {
+    path: "info",
+    component: InfoComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,31 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { DemoComponent } from './demo/demo.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { InfoComponent } from './info/info.component';
+import { UserComponent } from './user/user.component';
+import { UserdetailsComponent } from './userdetails/userdetails.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
+    DemoComponent,
+
+    InfoComponent,
+
+    UserComponent,
+
+    UserdetailsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
